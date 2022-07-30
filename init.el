c98ba925d7aa2688211c91607ed6bb58c72931e1
Trumae <trumae@gmail.com>
2022-07-29 21:58:35 -0300

    ??

--- init.el
+++ init.el
@@ -26,17 +26,79 @@
   (setq use-package-always-ensure t
         use-package-expand-minimally t))
 
+(require 'use-package)
 
-(use-package lsp-mode)
-;;(use-package markdown-mode)
-;;(use-package rustic)
+;;;;;;;;;;;;;;;; C/C++ ;;;;;;;;;;;;;;;
+(require 'cc-mode)
+(require 'semantic)
 
-;(use-package 'yasnippet
-;  :ensure t)
+(global-semanticdb-minor-mode 1)
+(global-semantic-idle-scheduler-mode 1)
+(global-semantic-stickyfunc-mode 1)
 
+(semantic-mode 1)
+
+(defun alexott/cedet-hook ()
+  (local-set-key "\C-c\C-j" 'semantic-ia-fast-jump)
+  (local-set-key "\C-c\C-s" 'semantic-ia-show-summary))
+
+(add-hook 'c-mode-common-hook 'alexott/cedet-hook)
+(add-hook 'c-mode-hook 'alexott/cedet-hook)
+(add-hook 'c++-mode-hook 'alexott/cedet-hook)
+
+;; Enable EDE only in C/C++
+(require 'ede)
+(global-ede-mode)
+
+;; Setup irony
+(use-package irony)
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+;; Setup company-mode
+(require 'company)
+(require 'irony)
+
+(use-package company-irony-c-headers
+  :config
+  (add-to-list
+   'company-backends '(company-irony-c-headers company-irony)))
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; install gnu global: `apt-get install global`
+(use-package ggtags
+  :init
+  (add-hook 'c-mode-common-hook
+            (lambda ()
+              (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
+		(ggtags-mode 1)))))
+(define-key ggtags-mode-map (kbd "C-c g s") 'ggtags-find-other-symbol)
+(define-key ggtags-mode-map (kbd "C-c g h") 'ggtags-view-tag-history)
+(define-key ggtags-mode-map (kbd "C-c g r") 'ggtags-find-reference)
+(define-key ggtags-mode-map (kbd "C-c g f") 'ggtags-find-file)
+(define-key ggtags-mode-map (kbd "C-c g c") 'ggtags-create-tags)
+(define-key ggtags-mode-map (kbd "C-c g u") 'ggtags-update-tags)
+(define-key ggtags-mode-map (kbd "M-,") 'pop-tag-mark)
+
+
+(use-package lsp-mode
+  :init
+  ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
+  (setq lsp-keymap-prefix "C-c l")
+  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
+         (python-mode . lsp)
+         ;; if you want which-key integration
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+(use-package markdown-mode)
+(use-package rustic)
+(use-package yasnippet
+  :ensure t)
 (use-package flycheck)
 (use-package projectile)
-
 (use-package go-eldoc)
 (use-package go-errcheck)
 (use-package go-mode)
@@ -100,12 +162,37 @@
    (js . t)
    (sqlite . t)
    (scheme .t)
+   (python . t)
+   (forth . t)
+   (makefile . t)
+   (perl . t)
+   (shell . t)
+   (ditaa . t)
    (C . t) 
    (dot . t)))
 
 (use-package ack)
 (use-package google-translate)
 (use-package gruvbox-theme)
+(use-package elpy
+  :ensure t
+  :init
+  (elpy-enable))
+(use-package tree-sitter :ensure t)
+(use-package tree-sitter-langs :ensure t)
+(use-package tree-sitter-indent :ensure t)
+(use-package csharp-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("\\.cs\\'" . csharp-tree-sitter-mode)))
+
+;(use-package centaur-tabs
+;  :demand
+;  :config
+;  (centaur-tabs-mode t)
+;  :bind
+;  ("C-<prior>" . centaur-tabs-backward)
+;  ("C-<next>" . centaur-tabs-forward))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -113,7 +200,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ob-tangle tuareg haskell-mode websocket use-package rustic projectile magit lua-mode lsp-mode gruvbox-theme google-translate go-errcheck go-eldoc flycheck erc cpupower beacon auctex ada-ref-man ada-mode ack))
+   '(cmake-ide ob-tangle tuareg haskell-mode websocket use-package rustic projectile magit lua-mode lsp-mode gruvbox-theme google-translate go-errcheck go-eldoc flycheck erc cpupower beacon auctex ada-ref-man ada-mode ack))
  '(warning-suppress-types
    '((comp)
      (comp)
